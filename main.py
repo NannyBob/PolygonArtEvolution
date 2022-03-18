2460b598880549068fe2e8445d63bf80932c69e2
lucas <L.G.Toner2@newcastle.ac.uk>
2022-03-18 17:49:04 +0000

    Added mutation.py And some changes

--- main.py
+++ main.py
@@ -3,37 +3,49 @@
 
 from evol import Population, Evolution
 import random
+import fitness
+import mutation
+
+init_polygons = 50
+
+
+def random_colour():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+
+def random_point():
+    return random.randint(0, 200), random.randint(0, 200)
+
+
+def rand_polygon():
+    # [(R,G,B,A),p1,p2,p3]
+    return [random_colour(), random_point(), random_point(), random_point()]
+
+
 
 
 def initialize():
-    # [(R,G,B,A),p1,p2,p3]
-    return [random_colour(), random_point(),random_point(), random_point()]
-
-def evaluate(x):
-    return sum(x)
+    return [rand_polygon() for i in range(init_polygons)]
 
 
-def select(population):
-    return [random.choice(population) for i in range(2)]
+def select(pop):
+    return [random.choice(pop) for _ in range(2)]
 
 
 def combine(*parents):
     return [a if random.random() < 0.5 else b for a, b in zip(*parents)]
 
 
-def flip(x, rate):
-    return [1 ^ i if random.random() < rate else i for i in x]
-
-
-population = Population.generate(initialize, evaluate, size=10, maximize=True)
+population = Population.generate(initialize, fitness.evaluate, size=10, maximize=True)
 population.evaluate()
 
-evolution = (Evolution().survive(fraction=0.5)
+evolution = (Evolution()
+             .survive(fraction=0.5)
              .breed(parent_picker=select, combiner=combine)
-             .mutate(mutate_function=flip, rate=0.1)
+             .mutate(mutate_function=mutation.mutate)
              .evaluate())
 
-for i in range(50):
+for i in range(100):
     population = population.evolve(evolution)
     print("i =", i, " best =", population.current_best.fitness,
           " worst =", population.current_worst.fitness)
---
+++ mutation.py
@@ -0,0 +1,24 @@
+import random
+import main
+
+
+def remove_polygon(solution):
+    if len(solution > 1):
+        return solution.pop(random.randint(0, len(solution) - 1))
+    return solution
+
+
+def add_polygon(solution):
+    solution.append(main.rand_polygon())
+    return solution
+
+def add_point(solution):
+    # http://homepages.cs.ncl.ac.uk/pawel.widera/2034/evolution/slides/art.html#/representation/0
+    polygon = random.choice(solution)
+
+def mutate(solution):
+    if random.random() < 0.5:
+        remove_polygon(solution)
+    else:
+        add_polygon(solution)
+    return solution
