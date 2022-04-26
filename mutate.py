0d815e481c9c1d6c463834c36e0d5394166d10fc
lucas <L.G.Toner2@newcastle.ac.uk>
2022-04-26 10:42:09 +0100

    Added mutate_add_similar_polygon

--- mutate.py
+++ mutate.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import config
 import create
 import random
@@ -14,7 +16,8 @@ def mutate(solution):
     if random.random() < mutation_probs["change colour"]:
         mutate_colour_gaussian(solution)
     if random.random() < mutation_probs["add polygon"]:
-        mutate_add_polygon(solution)
+        mutate_add_similar_polygon(solution)
+        #mutate_add_polygon(solution)
     if random.random() < mutation_probs["remove polygon"]:
         mutate_remove_polygon(solution)
     if random.random() < mutation_probs["add point"]:
@@ -28,57 +31,65 @@ def mutate(solution):
 # Maybe mutate colour and transparency seperately
 def mutate_colour_gaussian(solution):
     polygon = random.choice(solution)
-    polygon[0] = single_colour_gaussian(polygon[0])
-    return solution
+    polygon[0] = single_colour_gaussian(polygon[0], 10)
 
 
 def mutate_point_gaussian(solution):
     polygon = random.choice(solution)
     for i in range(len(polygon[1:])):
         polygon[i + 1] = single_point_gaussian(polygon[i + 1], 10)
-    return solution
 
 
 def mutate_add_polygon(solution):
     if len(solution) < config.config["max polygons"]:
         solution.append(create.random_polygon())
-    return solution
+
+
+def mutate_add_similar_polygon(solution):
+    if len(solution) < config.config["max polygons"]:
+        polygon = random.choice(solution)
+        new_polygon = deepcopy(polygon)
+        new_colour = single_colour_gaussian(polygon[0], 10)
+        #with low alpha channel
+        new_adjusted_colour = (new_colour[0],new_colour[1],new_colour[2],10)
+        new_polygon[0] = new_adjusted_colour
+
+        for count, point in enumerate(new_polygon[1:]):
+            new_polygon[count + 1] = single_point_gaussian(point, 10)
+        solution.append(new_polygon)
 
 
 def mutate_remove_polygon(solution):
     if len(solution) > config.config["min polygons"]:
         solution.pop(random.randint(0, len(solution) - 1))
-    return solution
 
 
 def mutate_add_point(solution):
     polygon = random.choice(solution)
     if len(polygon) - 1 < config.config["max points"]:
         centroid = fitness.find_centroid(polygon)
-        solution.append(single_point_gaussian(centroid, 40))
-    return solution
+        polygon.append(single_point_gaussian(centroid, 10))
 
 
 def mutate_remove_point(solution):
     polygon = random.choice(solution)
-    if len(polygon) -1 > config.config["min points"]:
-        solution.pop(random.randint(config.config["min points"], len(polygon) - 1))
-    return solution
+    if len(polygon) - 1 > config.config["min points"]:
+        polygon.pop(random.randint(config.config["min points"], len(polygon) - 1))
 
 
-def single_point_gaussian(point, move_range):
+def single_point_gaussian(point, sigma):
     x = point[0]
     y = point[1]
     # if random.random() < indpb:
-    x += random.gauss(0, move_range)
+    x += random.gauss(0, sigma)
     x = max(0, min(int(x), 200))
     # if random.random() < indpb:
-    y += random.gauss(0, move_range)
+    y += random.gauss(0, sigma)
     y = max(0, min(int(y), 200))
     return (x, y)
 
 
-def single_colour_gaussian(colour):
+def single_colour_gaussian(colour, sigma):
     r = colour[0]
     g = colour[1]
     b = colour[2]
