cfc2961634b7c79efe392d2740e12a96ec5a2dac
lucas <L.G.Toner2@newcastle.ac.uk>
2022-05-04 12:29:49 +0100

    Added Arithmetic CrossOver

--- breed.py
+++ breed.py
@@ -4,7 +4,7 @@ import config
 
 
 def breed(*parents):
-    return combine_pairs(*parents)
+    return arithmetic_crossover(*parents)
 
 
 def combine_pairs(*parents):
@@ -25,6 +25,50 @@ def combine_pairs(*parents):
                 to_return.append(polygon)
 
     return to_return
+
+
+def arithmetic_crossover(*parents):
+    child = []
+    len0 = len(parents[0])
+    len1 = len(parents[1])
+    for i in range(min(len0, len1)):
+        child.append(polygon_combine(parents[0][i], parents[1][i]))
+    if len0 > len1:
+        for polygon in parents[0][len1 - 1:]:
+            if random.random() > 0.5:
+                child.append(polygon)
+    else:
+        for polygon in parents[1][len0 - 1:]:
+            if random.random() > 0.5:
+                child.append(polygon)
+    return child
+
+
+def polygon_combine(a, b):
+    red = round((a[0][0] + b[0][0]) / 2)
+    green = round((a[0][1] + b[0][1]) / 2)
+    blue = round((a[0][2] + b[0][2]) / 2)
+    alpha = round((a[0][3] + b[0][3]) / 2)
+    lena = len(a[1:])
+    lenb = len(b[1:])
+    child = [(red, green, blue, alpha)]
+    for i in range(min(lena, lenb)):
+        child.append(point_combine(a[i + 1], b[i + 1]))
+    if lena > lenb:
+        for point in a[lenb:]:
+            if random.random() > 0.5:
+                child.append(point)
+    else:
+        for point in b[lena:]:
+            if random.random() > 0.5:
+                child.append(point)
+    return child
+
+
+def point_combine(a, b):
+    x = round((a[0] + b[0]) / 2)
+    y = round((a[1] + b[1]) / 2)
+    return x, y
 
 
 def combine_random(*parents):
